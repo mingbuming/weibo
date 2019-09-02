af9c0705203cac083b4cd73989cbec00758ffadc
mingbuming <mingbuming@126.com>
2019-09-02 06:43:54 +0000

    切割头部和尾部子视图

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,22 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>@yield('title', 'Weibo App') - Laravel 新手入门教程</title>
-    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>@yield('title', 'Weibo App') - Laravel 新手入门教程</title>
+  <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 </head>
 <body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="container">
-            <a class="navbar-brand" href="/">Webo App</a>
-            <ul class="navbar-nav justify-content-end">
-                <li class="nav-item"><a class="nav-link" href="/help">帮助</a></li>
-                <li class="nav-item"><a  class="nav-link" href="#">登录</a></li>
-            </ul>
-        </div>
-    </nav>
-    @yield('content')
+  @include('layouts._header')
+
+  <div class="container">
+    <div class="offset-md-1 col-md-10">
+      @yield('content')
+      @include('layouts._footer')
+      
+    </div>
+  </div>
 </body>
 </html>
